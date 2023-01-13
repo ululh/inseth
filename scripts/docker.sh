e371253e7264f09d9af6af67a19db8dfcaa4c4da
ululh <ulul@ulul.org>
2023-01-13 17:13:42 +0100

    functions in docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -22,21 +22,28 @@ then
 fi
 
 # build and run containers
-# SUDO_USER OK after sudo su but not sudo su -, where is RUID ??
-cd $(getent passwd $SUDO_USER | cut -d: -f6)/inseth/repo
 
-echo "Building collector...."
-docker build -f ethereum_collector/Dockerfile -t eth_client_img .
+build () {
 
-echo "Starting collector...."
-CONTAINER_ID=$(docker ps -a |grep eth_client | awk '{print $1}')
-if ! -z $CONTAINER_ID
-then
-docker rm $CONTAINER_ID
-fi
-docker run -d -v inseth:/data --name eth_client eth_client_img
+	# SUDO_USER OK after sudo su but not sudo su -, where is RUID ??
+	cd $(getent passwd $SUDO_USER | cut -d: -f6)/inseth/repo
+	echo "Building $1 ...."
+	docker build -f $1/Dockerfile -t ${1}_img .
+}
 
+run () {
 
+	CONTAINER_ID=$(docker ps -a |grep $1 | awk '{print $1}')
+	if [ ! -z $CONTAINER_ID ]
+	then
+		docker rm --force $CONTAINER_ID
+	fi
+	echo "Starting $1...."
+	docker run -d -v inseth:/data --name $1 ${1}_img
+}
+
+build ethereum_collector
+run ethereum_collector
 
 # troubleshoot
 # docker run -it --entrypoint bash eth_client_img
